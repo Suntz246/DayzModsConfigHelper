66b95b84df7ec56e298977e0cf4738bc2392fd0d
Sudo <3338883377@qq.com>
2024-02-15 13:22:37 +0800

    修复了极端情况下的比如：@Perks & Skills System [Version 2] (RELIFE)这种模组名字导致的生成mods配置文件失败

--- Step2-CreatModsConf.bat
+++ Step2-CreatModsConf.bat
@@ -9,11 +9,14 @@ set "folders="
 rem 遍历当前文件夹下的所有文件夹
 for /d %%i in (*) do (
     rem 将文件夹名称添加到 folders 变量中
-    set "folders=!folders!mods\%%i;"
+    set "folder=mods\%%i"
+    rem 修复特殊字符的处理
+    set "folder=!folder:"=^"!"
+    set "folders=!folders!!folder!;"
 )
 
 rem 将 folders 的内容写入 mods.txt 文件，内容开头添加 -mod=
-echo -mod=%folders:~0,-1% > mods.txt
+echo -mod=!folders:~0,-1! > mods.txt
 
 endlocal
 echo Step2 is Ok
